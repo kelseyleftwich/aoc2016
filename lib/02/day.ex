c39d180f24d99b18a94b58002d615f8b77061aca
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2023-11-01 09:48:13 -0500

    refactors part 1 to make it work for part 2

--- lib/02/day.ex
+++ lib/02/day.ex
@@ -1,4 +1,27 @@
 defmodule Day02 do
+  # 1 2 3
+  # 4 5 6
+  # 7 8 9
+
+  #     1
+  #   2 3 4
+  # 5 6 7 8 9
+  #   A B C
+  #     D
+  def get_keypad() do
+    [
+      %{key: 1, down: 4, right: 2},
+      %{key: 2, down: 5, left: 1, right: 3},
+      %{key: 3, down: 6, left: 2},
+      %{key: 4, up: 1, down: 7, right: 5},
+      %{key: 5, up: 2, down: 8, left: 4, right: 6},
+      %{key: 6, up: 3, down: 9, left: 5},
+      %{key: 7, up: 4, right: 8},
+      %{key: 8, up: 5, left: 7, right: 9},
+      %{key: 9, up: 6, left: 8}
+    ]
+  end
+
   def solve(filename) do
     cwd = Path.dirname(__ENV__.file)
 
@@ -7,6 +30,24 @@ defmodule Day02 do
       |> InputProc.get_file_stream(cwd)
       |> Stream.map(&String.trim/1)
       |> Stream.map(&String.graphemes/1)
+      |> Stream.map(fn line ->
+        line
+        |> Enum.map(fn char ->
+          case char do
+            "L" ->
+              :left
+
+            "R" ->
+              :right
+
+            "U" ->
+              :up
+
+            "D" ->
+              :down
+          end
+        end)
+      end)
       |> Enum.to_list()
 
     instructions
@@ -28,107 +69,22 @@ defmodule Day02 do
       |> String.to_integer()
 
     next_key =
-      process_line(next_line, start_key)
+      process_instruction(next_line, start_key)
 
     process_instructions(rest, passcode <> Integer.to_string(next_key))
   end
 
-  def process_line([], start_key) do
-    start_key
+  def process_instruction([], current_key) do
+    current_key
   end
 
-  def process_line([next_step | rest], start_key) do
-    next_key =
-      process_step(next_step, start_key)
+  def process_instruction([instruction | rest], current_key) do
+    keypad = get_keypad()
 
-    process_line(rest, next_key)
-  end
+    key = Enum.find(keypad, fn x -> x.key == current_key end)
 
-  def process_step("U", start_key) do
-    start_key
-    |> Kernel.-(3)
-    |> max(min_for_column(start_key))
-  end
+    next_key = Map.get(key, instruction, current_key)
 
-  def process_step("D", start_key) do
-    start_key
-    |> Kernel.+(3)
-    |> min(max_for_column(start_key))
-  end
-
-  def process_step("L", start_key) do
-    start_key
-    |> Kernel.-(1)
-    |> max(min_for_row(start_key))
-  end
-
-  def process_step("R", start_key) do
-    start_key
-    |> Kernel.+(1)
-    |> min(max_for_row(start_key))
-  end
-
-  def max_for_column(start_key) do
-    case start_key do
-      1 -> 7
-      2 -> 8
-      3 -> 9
-      4 -> 7
-      5 -> 8
-      6 -> 9
-      7 -> 7
-      8 -> 8
-      9 -> 9
-    end
-  end
-
-  def min_for_column(start_key) do
-    case start_key do
-      1 -> 1
-      2 -> 2
-      3 -> 3
-      4 -> 1
-      5 -> 2
-      6 -> 3
-      7 -> 1
-      8 -> 2
-      9 -> 3
-    end
-  end
-
-  def min_for_row(start_key) do
-    case start_key do
-      1 -> 1
-      2 -> 1
-      3 -> 1
-      4 -> 4
-      5 -> 4
-      6 -> 4
-      7 -> 7
-      8 -> 7
-      9 -> 7
-    end
-  end
-
-  def max_for_row(start_key) do
-    case start_key do
-      1 -> 3
-      2 -> 3
-      3 -> 3
-      4 -> 6
-      5 -> 6
-      6 -> 6
-      7 -> 9
-      8 -> 9
-      9 -> 9
-    end
+    process_instruction(rest, next_key)
   end
 end
-
-# 1 2 3
-# 4 5 6
-# 7 8 9
-
-# 5 -> 2
-# 6 -> 3
-# 4 -> 1
