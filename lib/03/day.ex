f050494ef48ea51312b243e1666e4a572f5deb8c
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2023-11-02 06:41:04 -0500

    day 3 - part 2

--- lib/03/day.ex
+++ lib/03/day.ex
@@ -1,5 +1,5 @@
 defmodule Day03 do
-  def solve(filename) do
+  def part_1(filename) do
     cwd = Path.dirname(__ENV__.file)
 
     filename
@@ -12,6 +12,30 @@ defmodule Day03 do
         String.to_integer(char)
       end)
     )
+    |> Stream.map(&Enum.sort/1)
+    |> Stream.filter(fn [a, b, c] ->
+      a + b > c
+    end)
+    |> Enum.count()
+  end
+
+  def part_2(filename) do
+    cwd = Path.dirname(__ENV__.file)
+
+    filename
+    |> InputProc.get_file_stream(cwd)
+    |> Stream.map(&String.trim(&1, "\n"))
+    |> Stream.map(&split_every_5_chars/1)
+    |> Stream.map(
+      &Enum.map(&1, fn char ->
+        char = String.trim(char)
+        String.to_integer(char)
+      end)
+    )
+    |> Stream.chunk_every(3)
+    |> Stream.flat_map(fn [[a1, b1, c1], [a2, b2, c2], [a3, b3, c3]] ->
+      [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]]
+    end)
     |> Stream.map(&Enum.sort/1)
     |> Stream.filter(fn [a, b, c] ->
       a + b > c
--- test/day03_test.exs
+++ test/day03_test.exs
@@ -2,10 +2,18 @@ defmodule Day03Test do
   use ExUnit.Case
 
   test "part 1 - example 1" do
-    assert Day03.solve("example.txt") === 0
+    assert Day03.part_1("example.txt") === 0
   end
 
   test "part 1 - input" do
-    assert Day03.solve("input.txt") === 983
+    assert Day03.part_1("input.txt") === 983
+  end
+
+  test "part 2 - example 2" do
+    assert Day03.part_2("example2.txt") === 6
+  end
+
+  test "part 2 - input " do
+    assert Day03.part_2("input.txt") === 1836
   end
 end
