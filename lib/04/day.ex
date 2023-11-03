c836df5a0b0fcc62169f397f20aa4994630bdd9e
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2023-11-03 06:55:14 -0500

    day 4 - part 2

--- lib/04/day.ex
+++ lib/04/day.ex
@@ -1,5 +1,14 @@
 defmodule Day04 do
   def part_1(filename) do
+    filename
+    |> get_valid_rooms()
+    |> Stream.map(fn {_, sector, _, _} ->
+      sector
+    end)
+    |> Enum.sum()
+  end
+
+  def get_valid_rooms(filename) do
     cwd = Path.dirname(__ENV__.file)
 
     filename
@@ -25,14 +34,55 @@ defmodule Day04 do
         |> Enum.map(fn {k, _} -> k end)
         |> Enum.join()
 
-      {room, sector, checksum}
+      sector =
+        sector
+        |> String.to_integer()
+
+      {room, sector, checksum, room_and_sector}
     end)
-    |> Stream.filter(fn {room, _, checksum} ->
+    |> Stream.filter(fn {room, _, checksum, _} ->
       room == checksum
     end)
-    |> Stream.map(fn {_, sector, _} ->
-      String.to_integer(sector)
+  end
+
+  def part_2(filename) do
+    filename
+    |> get_valid_rooms()
+    |> Stream.map(fn {_, sector, _, room_and_sector} ->
+      {room, _} = String.split_at(room_and_sector, -4)
+
+      parsed =
+        room
+        |> String.graphemes()
+        |> Enum.map(fn character ->
+          shift_char(character, sector)
+        end)
+        |> Enum.join()
+
+      {parsed, sector}
     end)
-    |> Enum.sum()
+    |> Stream.filter(fn {parsed, sector} ->
+      parsed
+      |> String.contains?("north")
+    end)
+    |> Enum.to_list()
+  end
+
+  @alphabet "abcdefghijklmnopqrstuvwxyz" |> String.graphemes()
+
+  def shift_char("-", _), do: "-"
+
+  def shift_char(character, shift_count) do
+    char_index =
+      @alphabet
+      |> Enum.find_index(&(&1 == character))
+
+    target_index =
+      char_index
+      |> Kernel.+(shift_count)
+      |> Kernel.rem(26)
+
+    @alphabet
+    |> Enum.at(target_index)
   end
 end
--- test/day04_test.exs
+++ test/day04_test.exs
@@ -8,4 +8,8 @@ defmodule Day04Test do
   test "part 1 - input 1" do
     assert Day04.part_1("input.txt") === 173_787
   end
+
+  test "part 2 - input" do
+    assert [{_, 548}] = Day04.part_2("input.txt")
+  end
 end
