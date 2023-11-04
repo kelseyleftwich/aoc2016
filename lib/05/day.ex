3497c3f1597d4f71c2d5c5f5c568cf81a2c0268a
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2023-11-04 08:32:17 -0500

    day 5 - part 2

--- lib/05/day.ex
+++ lib/05/day.ex
@@ -25,4 +25,53 @@ defmodule Day05 do
 
     part_1(door, iter + 1, passcode)
   end
+
+  def part_2(door_id) do
+    part_2(door_id, 0, [nil, nil, nil, nil, nil, nil, nil, nil])
+  end
+
+  def part_2(_door_id, _iter, passcode)
+      when is_binary(passcode) and byte_size(passcode) == 8 do
+    passcode
+  end
+
+  def part_2(door, iter, passcode) do
+    passcode
+    |> Enum.any?(fn char -> char == nil end)
+    |> case do
+      true ->
+        code = "#{door}#{iter}"
+
+        passcode =
+          :crypto.hash(:md5, code)
+          |> Base.encode16(case: :lower)
+          |> String.slice(0..6)
+          |> case do
+            "00000" <> chars ->
+              [pos_char, insert_char] =
+                chars |> String.graphemes()
+
+              case Integer.parse(pos_char) do
+                :error ->
+                  passcode
+
+                {pos_char, _} ->
+                  if Enum.at(passcode, pos_char) == nil do
+                    passcode
+                    |> List.replace_at(pos_char, insert_char)
+                  else
+                    passcode
+                  end
+              end
+
+            _ ->
+              passcode
+          end
+
+        part_2(door, iter + 1, passcode)
+
+      false ->
+        passcode |> Enum.join()
+    end
+  end
 end
--- test/day05_test.exs
+++ test/day05_test.exs
@@ -8,4 +8,12 @@ defmodule Day05Test do
   test "part 1 - input 1" do
     assert Day05.part_1("ffykfhsq") == "c6697b55"
   end
+
+  test "part 2 - example 1" do
+    assert Day05.part_2("abc") == "05ace8e3"
+  end
+
+  test "part 2 - input 1" do
+    assert Day05.part_2("ffykfhsq") == "8c35d1ab"
+  end
 end
