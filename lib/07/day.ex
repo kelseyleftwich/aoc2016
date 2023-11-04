327d4f362a393436ca58176e8e776ac849d006f3
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2023-11-04 13:09:33 -0500

    day 7 - part 2

--- lib/07/day.ex
+++ lib/07/day.ex
@@ -1,5 +1,5 @@
 defmodule Day07 do
-  def solve(filename) do
+  def part_1(filename) do
     cwd = Path.dirname(__ENV__.file)
 
     filename
@@ -38,6 +38,77 @@ defmodule Day07 do
       unwrapped_valid && wrapped_valid
     end)
     |> Enum.count()
+  end
+
+  def part_2(filename) do
+    cwd = Path.dirname(__ENV__.file)
+
+    filename
+    |> InputProc.get_file_stream(cwd)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(fn line ->
+      parts =
+        line
+        |> String.replace("]", "[")
+        |> String.split("[")
+
+      unwrapped =
+        parts
+        |> Enum.take_every(2)
+
+      wrapped =
+        parts
+        |> tl()
+        |> Enum.take_every(2)
+
+      {unwrapped, wrapped}
+    end)
+    |> Stream.map(fn {unwrapped, wrapped} ->
+      unwrapped =
+        unwrapped
+        |> get_palindromes()
+
+      wrapped =
+        wrapped
+        |> get_palindromes()
+
+      {unwrapped, wrapped}
+    end)
+    |> Stream.filter(fn {unwrapped, wrapped} ->
+      wrapped =
+        wrapped
+        |> Enum.map(fn {a, b} ->
+          {b, a}
+        end)
+
+      wrapped
+      |> Enum.any?(fn {a, b} ->
+        unwrapped
+        |> Enum.any?(fn {c, d} ->
+          a == c && b == d
+        end)
+      end)
+    end)
+    |> Enum.count()
+  end
+
+  def get_palindromes(input) do
+    input
+    |> Enum.map(fn line ->
+      line
+      |> String.graphemes()
+      |> Enum.chunk_every(3, 1, :discard)
+      |> Enum.filter(fn [a, b, c] ->
+        a == c && a != b
+      end)
+      |> Enum.map(fn [a, b, _] ->
+        {a, b}
+      end)
+      |> Enum.uniq()
+    end)
+    |> Enum.filter(fn x -> x != [] end)
+    |> Enum.concat()
+    |> Enum.uniq()
   end
 
   def filter_abba([_, true, _]) do
--- test/day07_test.exs
+++ test/day07_test.exs
@@ -1,11 +1,19 @@
 defmodule Day07Test do
   use ExUnit.Case
 
-  test "part 1 - example 1" do
-    assert Day07.solve("example.txt") == 2
+  test "part 1 - example" do
+    assert Day07.part_1("example.txt") == 2
   end
 
   test "part 1 - input" do
-    assert Day07.solve("input.txt") == 115
+    assert Day07.part_1("input.txt") == 115
+  end
+
+  test "part 2 - example " do
+    assert Day07.part_2("example2.txt") == 3
+  end
+
+  test "part 2 - input " do
+    assert Day07.part_2("input.txt") == 231
   end
 end
